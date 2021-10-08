cf724cca16a025731bb5fbb99f7f495c75504650
dporahau <DPorahau@gomel.iba.by>
2021-10-08 08:13:33 +0300

    create labs for js

---
+++ js/lab2/2.js
@@ -0,0 +1,6 @@
+var n = parseInt(prompt("Input size of matrix"));
+let arr = [];
+
+for( let i = 0; i < n; i++) arr.push(parseInt(prompt("array["+i+"]")));
+
+alert("array --> " + arr);
---
+++ js/lab3/3.js
@@ -0,0 +1,11 @@
+let word = prompt("Input a word");
+
+function reverseString(str) {
+    let reverse = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
+    return reverse;
+}
+
+document.write(reverseString(word))
