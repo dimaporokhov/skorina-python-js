4cf1cbea5a5c6a2e6bd0c06f8f023326facd8c59
dporahau <DPorahau@gomel.iba.by>
2021-10-25 10:18:05 +0300

    mtfr

---
+++ mtfr/mtfr.py
@@ -0,0 +1,75 @@
+from math import sqrt
+
+# вероятность
+p = 0.01
+
+# количество людей
+N = [10000, 5000, 4000, 1000]
+
+# сумма контракта
+B = [10000, 20000, 50000, 100000]
+
+# относительная страховая надбавка
+Q = 0.15
+
+# предел удержания при перестраховании в рублях
+r = 50000
+
+Q_ = 0.20
+
+################################ до перестрахования
+print(f"B(i) --> {B}")
+
+MX = [N[i] * p * B[i] for i in range(len(N))]
+print(f"MX(i) --> {MX}")
+
+MS = sum(MX)
+print(f"MS --> {MS}")
+
+DX_gr = [N[i] * (p * B[i]**2 - (p * B[i])**2) for i in range(len(N))]
+print(f"DX(i) --> {DX_gr}")
+
+U = (1 + Q) * MS
+print(f"U --> {U}")
+
+I = U - MS
+print(f"I --> {I}")
+
+DS = sum(DX_gr)
+print(f"DS --> {DS}")
+R = f"1 - N({I / sqrt(DS)})"
+print(f"R --> {R}")
+
+################################ после перестрахования
+B_after = []
+i_after = []
+for i, el in enumerate(B):
+    if el > r:
+        B_after.append(el - r)
+        i_after.append(i)
+    else:
+        B_after.append(el)
+print(f"\nB(i)* --> {B_after}")
+
+MX_after = [N[i] * p * B_after[i] for i in range(len(N))]
+print(f"MX(i)* --> {MX_after}")
+
+MS_after = sum(MX_after)
+print(f"MS* --> {MS_after}")
+
+DX_gr_after = [N[i] * (p * B_after[i]**2 - (p * B_after[i])**2) for i in range(len(N))]
+print(f"DX(i)* --> {DX_gr_after}")
+
+P_ = (1 + Q_) * (MS - MS_after)
+print(f"P* --> {P_}")
+
+U_ = U - P_
+print(f"U* --> {U_}")
+
+I_ = U_ - MS_after
+print(f"I* --> {I_}")
+
+DS_after = sum(DX_gr_after)
+print(f"DS* --> {DS_after}")
+R_ = f"1 - N({I_ / sqrt(DS_after)})"
+print(f"R* --> {R_}")
